c80d158334743bb0bafa35caf95d528743e9be22
Ivaylo Atanasov <aivo@devlabs.bg>
2025-06-12 18:59:33 +0300

    adds erc20 address

--- evm-bridge-cli/src/commands/lock.ts
+++ evm-bridge-cli/src/commands/lock.ts
@@ -3,7 +3,8 @@ import { Command } from 'commander';
 export default function lockCommand(): Command {
   return new Command('lock')
     .description('Lock tokens via `permit` + bridge interaction')
-    .action(() => {
+    .action(async () => {
+
       console.log('Tokens locked!');
     });
 }
